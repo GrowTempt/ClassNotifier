07f449e8ecc0c0d707cabd0f445acd5ee666436f
GrowTempt <vincenthoang111@gmail.com>
2024-02-15 22:42:22 -0800

    Added User Interface

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+# UCI Class Notifier
+import user_interface
+
+user_courses = []
+
+def main():
+    user_input = input()
+    while user_input != "Q":
+        user_interface.main_menu()
+        user_input = input()
+
+if __name__ == "__main__":
+    main()
---
+++ user_interface.py
@@ -0,0 +1,8 @@
+def main_menu():
+    print("Welcome to UCI Class Notifier!")
+    print()
+    print("Please type the desired option:")
+    print("\t1 - Add Course")
+    print("\t2 - Delete Course")
+    print("\t3 - View Added Courses")
+    print("\tQ - Quit Program")
